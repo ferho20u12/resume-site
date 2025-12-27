c01d139c16cd819ddf98c19ea7cc0107f300f151
Fernando Gutierrez <ferho20u12@gmail.com>
2025-12-27 00:47:45 -0600

    separate logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import PageContainer from "@src/shared/components/PageContainer";
+import Profile from "@src/app/profile/page";
 
 export default function HomePage() {
   return (
     <PageContainer>
-      <h1 className="text-2xl font-bold mb-4">Portada</h1>
-      <p>Bienvenido a mi portafolio!</p>
+      <Profile/>
     </PageContainer>
   );
 }
