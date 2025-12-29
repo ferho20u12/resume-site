0fc1b0552554f3cfc5d4054e760553cb3cf58872
Fernando Gutierrez <ferho20u12@gmail.com>
2025-12-28 19:56:28 -0600

    config to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   output: "export",
   reactCompiler: true,
 };
 
-export default nextConfig;
+// export default nextConfig;
+module.exports = nextConfig;
